83a27a5c80f86f1e8fca94d918f0cb363dd8510d
Rakshitha K <rakshitha@CLAP805.prodapt.com>
2025-11-17 16:41:55 +0530

    updated code

--- main.py
+++ main.py
@@ -22,5 +22,19 @@ async def intro():
 async def lap():
    return "laptop!"
 
+@app.get("/add")
+async def add(x=10,y=20):
+  res=x+y
+  return {"result":res}
+
+@app.get("/mul")
+async def mul(x=20,y=2):
+  res1=x*y
+  return {"result":res1}
+
+@app.get("div")
+async def div(x=10,y=2):
+  ans=x/y
+  return {"result":ans}
 
 
