e38fd680ee7c125d1c66d31ad2ec1d36a54306b2
Rakshitha K <rakshitha@CLAP805.prodapt.com>
2025-11-17 14:41:36 +0530

    rakshi

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ app = FastAPI()
 
 @app.get("/health")
 async def health():
-  return {"status": "ok"}
+  return {"status": "ok", "app_health": "healthy"}
+
+@app.get("/")
+async def route():
+  return {"Hello": "World"}
+
+@app.get("/doc")
+async def doc():
+  return "Hi DOCTOR!"
